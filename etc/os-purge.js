a001e6d8b8e39d8f823fccadedd58e54d9d99160
Luciano Bueno <glamero@gmail.com>
2018-02-20 18:57:51 -0300

    bump

---
+++ etc/os-purge.js
@@ -0,0 +1,54 @@
+module.exports = {
+ input: [
+	{
+		poll: {
+			id: "input.os.purge.cradle",
+			conn: [
+				{
+					scheme: 'cradle',
+					host:'192.168.0.180',
+					//host:'127.0.0.1',
+					port: 5984 ,
+					db: 'dashboard'}
+			],
+			requests: {
+				/**
+				 * runnign at 20 secs intervals
+				 * needs 3 runs to start analyzing from last stats (or from begining)
+				 * it takes 60 secs to complete, so it makes stats each minute
+				 * */
+				periodical: 10000,
+			},
+		},
+	}
+ ],
+ //filters: [
+		//require('./snippets/filter.os.statistics.template'),
+		//require('./snippets/filter.sanitize.template'),
+	//],
+	//output: [
+		////require('./snippets/output.stdout.template'),
+		//{
+			//cradle: {
+				//id: "output.os.stats.cradle",
+				//conn: [
+					//{
+						////host: '127.0.0.1',
+						//host: '192.168.0.180',
+						//port: 5984,
+						//db: 'stats',
+						//opts: {
+							//cache: true,
+							//raw: false,
+							//forceSave: true,
+						//}
+					//},
+				//],
+				//buffer:{
+					//size: 0,
+					//expire:0
+				//}
+			//}
+		//}
+	//]
+}
--- etc/pipelines.js
+++ etc/pipelines.js
@@ -1,5 +1,6 @@
 module.exports = [
     //require('./munin.js'),
-    require('./os'),
+    //require('./os'),
     //require('./os-stats'),
+    require('./os-purge'),
 ]
