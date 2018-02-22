f354313cf1d2c60721f258e8e2b97b8c77b3e03b
Luciano Bueno <glamero@gmail.com>
2018-02-22 00:37:09 -0300

    bump

--- etc/snippets/filter.os.hour.statistics.template.js
+++ etc/snippets/filter.os.hour.statistics.template.js
@@ -80,57 +80,57 @@ module.exports = function(doc, opts, next){
 					let speed = [];
 					let times = {};
 					//Array.each(value, function(sample){
-						//Array.each(sample, function(cpu, core){
-							////if(!speed[core]) speed[core] = [];
+						Array.each(value, function(cpu, core){
+							//if(!speed[core]) speed[core] = [];
 							
-							//debug_internals('os-hour-stats filter speed %o', cpu);
+							debug_internals('os-hour-stats filter speed %o', cpu);
 						
-							////speed[core].push(cpu.speed)
-							//speed.push(cpu.speed);
+							//speed[core].push(cpu.speed)
+							speed.push(cpu.speed);
 							
-							//let sample_time = {};
-							//Object.each(cpu.times, function(time, key){//user,nice..etc
-								//if(!times[key]) times[key] = [];
-								//times[key].push(time);
-							//});
+							let sample_time = {};
+							Object.each(cpu.times, function(time, key){//user,nice..etc
+								if(!times[key]) times[key] = [];
+								times[key].push(time);
+							});
 							
-						//});
+						});
 						
 					//});
 					
-					//Object.each(times, function(time, key){//user,nice..etc
+					Object.each(times, function(time, key){//user,nice..etc
+						let data = {
+							min : ss.min(time),
+							max : ss.max(time),
+							avg : ss.mean(time),
+							median : ss.median(time),
+						};
+						
+						times[key] = data;
+					});
+					
+					//Array.each(speed, function(cpu, core){//do the statictics
 						//let data = {
-							//min : ss.min(time),
-							//max : ss.max(time),
-							//avg : ss.mean(time),
-							//median : ss.median(time),
+							//min : ss.min(cpu),
+							//max : ss.max(cpu),
+							//avg : ss.mean(cpu),
+							//median : ss.median(cpu),
 						//};
 						
-						//times[key] = data;
+						//speed[core] = data;
 					//});
 					
-					////Array.each(speed, function(cpu, core){//do the statictics
-						////let data = {
-							////min : ss.min(cpu),
-							////max : ss.max(cpu),
-							////avg : ss.mean(cpu),
-							////median : ss.median(cpu),
-						////};
-						
-						////speed[core] = data;
-					////});
 					
-					
-					//new_doc['data'][key] = {
-						//samples: value,
-						//speed: {
-							//min : ss.min(speed),
-							//max : ss.max(speed),
-							//avg : ss.mean(speed),
-							//median : ss.median(speed),
-						//},
-						//times: times
-					//};
+					new_doc['data'][key] = {
+						samples: value,
+						speed: {
+							min : ss.min(speed),
+							max : ss.max(speed),
+							avg : ss.mean(speed),
+							median : ss.median(speed),
+						},
+						times: times
+					};
 				}
 				else{
 					new_doc['data'][key] = {
