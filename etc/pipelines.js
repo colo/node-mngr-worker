3027ac11473cc051f048da55382cb80871e0a8f0
Luciano Bueno <glamero@gmail.com>
2018-03-01 16:56:51 -0300

    bump

--- etc/pipelines.js
+++ etc/pipelines.js
@@ -1,7 +1,7 @@
 module.exports = [
     ////require('./munin.js'),
-    //require('./info.os'),
-    require('./os.minute.stats'),
+    require('./info.os'),
+    //require('./os.minute.stats'),
     ////require('./os.fix.stats'),
     //require('./os.hour.stats'),
     //require('./os.purge'),
