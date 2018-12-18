95fe6c1956e67a5ed867b9e329940709fb2cca89
Luciano Bueno <glamero@gmail.com>
2018-12-17 21:58:02 -0300

    bump

--- etc/pipelines.js
+++ etc/pipelines.js
@@ -1,11 +1,13 @@
 const path = require('path')
 
+let conn = require('./default.conn.js')()
+
 module.exports = [
     //require('./local/munin.js'),
     //
     require(path.join(process.cwd(), 'apps/os/info/pipeline')),
 
-    require(path.join(process.cwd(), 'apps/os/historical/minute/pipeline')),
+    require(path.join(process.cwd(), 'apps/os/historical/minute/pipeline'))(conn),
     require(path.join(process.cwd(), 'apps/os/historical/hour/pipeline')),
 
     require(path.join(process.cwd(), 'apps/os/alerts/pipeline')),
