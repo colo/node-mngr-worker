aba2dd4a4d80f33849030571d27b7421dadd9396
Luciano Bueno <glamero@gmail.com>
2019-04-27 18:16:05 -0300

    bump

--- etc/pipelines.js
+++ etc/pipelines.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
-let conn = require('./default.conn.js')()
+let conn = require('./default.conn')()
+let redis = require('./default.redis')
+let bbb_conn = require('./bbb.conn.js')()
 
 module.exports = [
     //require('./local/munin.js'),
-    //
+
+    //require(path.join(process.cwd(), 'apps/bbb/pipeline'))(bbb_conn),
+    
     require(path.join(process.cwd(), 'apps/os/pipeline')),
     require(path.join(process.cwd(), 'apps/munin/pipeline'))(conn),
     require(path.join(process.cwd(), 'apps/ui/pipeline'))(conn),
@@ -13,8 +17,8 @@ module.exports = [
     require(path.join(process.cwd(), 'apps/historical/hour/pipeline'))(conn),
 
     require(path.join(process.cwd(), 'apps/purge/periodical/pipeline'))(conn),
-    require(path.join(process.cwd(), 'apps/purge/ui/pipeline'))(conn),
     require(path.join(process.cwd(), 'apps/purge/historical/pipeline'))(conn),
+    require(path.join(process.cwd(), 'apps/purge/ui/pipeline'))(conn, redis),
 
 
     // require(path.join(process.cwd(), 'apps/os/alerts/pipeline')),
