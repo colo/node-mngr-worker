e0f01ff3ef0fdd0eb41c4c176769aff6f9375514
Luciano Bueno <glamero@gmail.com>
2019-08-15 17:45:19 -0300

    bump

--- etc/default.conn.js
+++ etc/default.conn.js
@@ -5,7 +5,7 @@ module.exports = function(){
   return {
     host: '127.0.0.1',
 		port: 28015,
-		db: 'servers',
+		db: 'production',
     // couchdb: {
     //   request: (redis) ? require('cachemachine')({redis: true, hostname: 'elk'}) : undefined
     // },
