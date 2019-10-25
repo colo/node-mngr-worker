a66967109cece1904d158d959b0663b3ba895f05
Luciano Bueno <glamero@gmail.com>
2019-10-24 21:52:10 -0300

    bump

--- etc/pipelines.js
+++ etc/pipelines.js
@@ -75,16 +75,16 @@ module.exports = [
     //   }
     // ),
     //
-    // require(path.join(process.cwd(), 'apps/educativa/purge/all/pipeline'))(
-    //   {
-    //     input: Object.merge(Object.clone(conn), {table: 'educativa'}),
-    //     output: Object.merge(Object.clone(conn), {table: 'educativa'}),
-    //     filters: Array.clone([
-    //       require(path.join(process.cwd(), 'apps/educativa/purge/filters/00_from_default_query_delete_until_last_hour')),
-    //     ]),
-    //     type: 'check'
-    //   }
-    // ),
+    require(path.join(process.cwd(), 'apps/educativa/purge/all/pipeline'))(
+      {
+        input: Object.merge(Object.clone(conn), {table: 'educativa'}),
+        output: Object.merge(Object.clone(conn), {table: 'educativa'}),
+        // filters: Array.clone([
+        //   require(path.join(process.cwd(), 'apps/educativa/purge/filters/00_from_default_query_delete_until_last_hour')),
+        // ]),
+        // type: 'check'
+      }
+    ),
 
 
 
