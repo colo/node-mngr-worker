5da944c255dbd5d69fdf1be562e93177512b3e4c
Luciano Bueno <glamero@gmail.com>
2020-01-25 22:17:48 -0300

    #0000 - bump (carrot / neataptic /brain.js test)

--- etc/pipelines.js
+++ etc/pipelines.js
@@ -9,6 +9,13 @@ const munin = require('./munin')
 const telegram = require('./telegram')
 const http_ui = require('./http.ui')
 
+const periodical_training_filters_carrot = [
+  // require(path.join(process.cwd(), 'apps/stat-changes/filters/00_from_changes_build_buffer')),
+  // require(path.join(process.cwd(), 'apps/stat-changes/filters/00_from_default_query_build_lasts')),
+  // require(path.join(process.cwd(), 'apps/stat-changes/filters/01_from_lasts_get_hour_historical_ranges')),
+  require(path.join(process.cwd(), 'apps/stat-changes/filters/02_from_buffer_train-carrot'))
+]
+
 const periodical_training_filters_changes = [
   require(path.join(process.cwd(), 'apps/stat-changes/filters/00_from_changes_build_buffer')),
   require(path.join(process.cwd(), 'apps/stat-changes/filters/02_from_buffer_train-written_docs-os_stats'))
@@ -95,6 +102,51 @@ module.exports = [
     //   }//
     // ),
 
+    /**
+    * Carrot
+    **/
+    require(path.join(process.cwd(), 'apps/stat-changes/periodical/pipeline'))(
+      {
+        input: Object.merge(
+          Object.clone(conn),
+          {
+            table: 'os',
+            type: 'hour',
+            full_range: false,
+            requests: {
+              req : {
+                'id': 'periodical',
+                'index': false,
+                query: {
+                  'q': [
+                    'data',
+                    { 'metadata': ['path', 'timestamp', 'host'] }
+                  ],
+                  'transformation': [
+                    {
+                      'orderBy': { 'index': 'r.desc(timestamp)' }
+                    }
+                  ],
+                  'filter': [
+                    { 'metadata': { 'host': 'elk' } },
+                    "this.r.row('metadata')('path').eq('os.cpus')" +
+                    ".or(this.r.row('metadata')('path').eq('os.blockdevices.vda3.time'))" +
+                    ".or(this.r.row('metadata')('path').eq('os.blockdevices.vda3.sectors'))" +
+                    ".or(this.r.row('metadata')('path').eq('os.rethinkdb.server.written_docs'))" +
+                    ".or(this.r.row('metadata')('path').eq('os.rethinkdb.server.read_docs'))"
+                  ]
+                }
+
+              }
+            }
+          }
+        ),
+        output: Object.merge(Object.clone(conn), {table: 'ml'}),
+        filters: Array.clone(periodical_training_filters_carrot),
+
+
+      }//
+    ),
 
     /**
     * OS
@@ -132,23 +184,23 @@ module.exports = [
     /**
     * OS Purge
     **/
-    require(path.join(process.cwd(), 'apps/purge/periodical/pipeline'))(
-      {
-        input: Object.merge(Object.clone(conn), {table: 'os'}),
-        output: Object.merge(Object.clone(conn), {table: 'os'}),
-        filters: Array.clone(periodical_purge_filters),
-        type: 'periodical'
-      }
-    ),
-
-    require(path.join(process.cwd(), 'apps/purge/periodical/pipeline'))(
-      {
-        input: Object.merge(Object.clone(conn), {table: 'os_historical'}),
-        output: Object.merge(Object.clone(conn), {table: 'os_historical'}),
-        filters: Array.clone(minute_purge_filters),
-        type: 'minute'
-      }
-    ),
+    // require(path.join(process.cwd(), 'apps/purge/periodical/pipeline'))(
+    //   {
+    //     input: Object.merge(Object.clone(conn), {table: 'os'}),
+    //     output: Object.merge(Object.clone(conn), {table: 'os'}),
+    //     filters: Array.clone(periodical_purge_filters),
+    //     type: 'periodical'
+    //   }
+    // ),
+    //
+    // require(path.join(process.cwd(), 'apps/purge/periodical/pipeline'))(
+    //   {
+    //     input: Object.merge(Object.clone(conn), {table: 'os_historical'}),
+    //     output: Object.merge(Object.clone(conn), {table: 'os_historical'}),
+    //     filters: Array.clone(minute_purge_filters),
+    //     type: 'minute'
+    //   }
+    // ),
 
     /**
     * OS tabular
