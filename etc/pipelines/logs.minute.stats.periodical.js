94cd26e065f76bb4ffa6bb411eabeb2259c51aa0
Luciano Bueno <glamero@gmail.com>
2020-03-12 20:12:03 -0300

    #0000 - bump

---
+++ etc/pipelines/logs.minute.stats.periodical.js
@@ -0,0 +1,45 @@
+const path = require('path')
+
+const conn = require('../default.conn')()
+
+const minute_stats_filters = [
+  require(path.join(process.cwd(), 'apps/stat-changes/filters/00_from_periodical_build_lasts')),
+  require(path.join(process.cwd(), 'apps/stat-changes/filters/01_from_lasts_get_minute_historical_ranges')),
+  require(path.join(process.cwd(), 'apps/stat-changes/filters/02_from_ranges_create_stats'))
+]
+
+
+let pipelines = [
+
+
+  /**
+  * Logs Stats (periodical)
+  **/
+  require(path.join(process.cwd(), 'apps/stat-changes/periodical/pipeline'))(
+    {
+      input: Object.merge(
+        Object.clone(conn), {
+          table: 'logs',
+          type: 'minute',
+          full_range: false,
+          requests: {
+            req : {
+              // query: {
+              //   index: 'path',
+              // },
+              'id': 'periodical',
+            }
+          }
+        }
+      ),
+      output: Object.merge(Object.clone(conn), {table: 'logs_historical'}),
+      filters: Array.clone(minute_stats_filters),
+
+    }
+
+  ),
+
+]
+
+
+module.exports = pipelines
