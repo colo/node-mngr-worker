537e653dd205d0613e5d1f2022544b727bf2a20a
Luciano Bueno <glamero@gmail.com>
2020-03-23 22:01:37 -0300

    #0000 - bump

--- etc/http.receiver.js
+++ etc/http.receiver.js
@@ -34,10 +34,18 @@ module.exports = {
 	// 	sendImmediately: false,
 	// },
   authentication: {
+    init: true,
     users : [
-        { id: 1, username: 'anonymous' , role: 'anonymous', password: ''}
+        { id: 1, username: 'mngr' , role: 'mngr', password: '1234'}
     ],
   },
+
+  // authorization: {
+	// 	config: {
+	// 		"permissions":[],
+	// 	},
+	// },
+
 	// logs: {
 	// 	loggers: {
 	// 		error: null,
