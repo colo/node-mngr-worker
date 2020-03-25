820bae1090b04c2fdb15c4a660040a29a2b35bbf
Luciano Bueno <glamero@gmail.com>
2020-03-24 21:40:52 -0300

    #0000 - bump

---
+++ etc/pipelines/remote/logs.nginx.js
@@ -0,0 +1,72 @@
+const path = require('path')
+
+const conn = require('../../default.conn')()
+const http_receiver = require('../../http.receiver')
+
+let pipelines = [
+
+  /**
+  * Logs
+  **/
+  // require(path.join(process.cwd(), 'apps/logs/web/pipeline'))(
+  //   {
+  //     input: {
+  //       file: path.join(process.cwd(), 'devel/var/log/nginx/www.educativa.com-access.log'),
+  //       stdin: true,
+  //       domain: 'www.educativa.com',
+  //     },
+  //     output: conn,
+  //     opts: {
+  //       type: 'nginx'
+  //     }
+  //   }
+  // ),
+
+
+]
+
+
+/**
+* load all access log from dir (production)
+**/
+
+const glob = require('glob')
+const os = require('os')
+const DIR = path.join(process.cwd(), 'devel/var/log/nginx/')
+// const DIR = '/var/log/nginx/'
+
+const files = glob.sync('*access.log', {
+  'cwd': DIR
+})
+
+Array.each(files, function(file){
+  /**
+  * Logs
+  **/
+
+  let domain = file.replace('-access.log', '')
+  domain = domain.replace('access.log', '')
+  domain = (domain === '') ? os.hostname() : domain
+
+  pipelines.push(
+    // require(path.join(process.cwd(), 'apps/logs/web/pipeline'))(
+    require(path.join(process.cwd(), 'apps/logs/web/pipeline-send-remote'))(
+      {
+        input: {
+          file: path.join(DIR, file),
+          stdin: false,
+          domain: domain
+        },
+        output: http_receiver,
+        opts: {
+          type: 'nginx'
+        }
+      }
+
+
+
+    )
+  )
+})
+
+module.exports = pipelines
